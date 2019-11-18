47bc23be0131100eb3c876efc426cd77b5558a21
Florient CHOUTEAU <florient.chouteau@gmail.com>
2019-11-18 14:24:19 +0100

    Update scripts to add toy & big dataset

--- teacher/generate_student_datasets.py
+++ teacher/generate_student_datasets.py
@@ -20,7 +20,6 @@
 import os
 import sys
 
-import numpy as np
 import pandas as pd
 import scipy.stats
 import tqdm
@@ -149,19 +148,31 @@ labels = item.labels
 # plt.show()
 
 # %% [markdown]
-# ## Toy Dataset Generation
-# Let's generate our first dataset
+# ## Dataset parsing using khumeia
 
 # %%
-trainval_dataset.items = trainval_dataset.items[:4]
+MAX_ITEMS = 4
+TILE_SIZE = 64
+SPARSE_TILE_STRIDE = 64
+DENSE_TILE_STRIDE = 16
+MARGIN = 16
+
+# %%
+trainval_dataset.items = trainval_dataset.items[:min(len(trainval_dataset), MAX_ITEMS)]
 train_dataset, test_dataset = helpers.dataset_generation.split_dataset(trainval_dataset, proportion=0.75)
 
 # %%
 from khumeia.roi.sliding_window import SlidingWindow
 
 # %%
-sliding_window_dense = SlidingWindow(tile_size=64, stride=64, margin_from_bounds=16, discard_background=True)
-sliding_window_sparse = SlidingWindow(tile_size=64, stride=64, margin_from_bounds=16, discard_background=False)
+sliding_window_dense = SlidingWindow(tile_size=TILE_SIZE,
+                                     stride=DENSE_TILE_STRIDE,
+                                     margin_from_bounds=MARGIN,
+                                     discard_background=True)
+sliding_window_sparse = SlidingWindow(tile_size=TILE_SIZE,
+                                      stride=SPARSE_TILE_STRIDE,
+                                      margin_from_bounds=MARGIN,
+                                      discard_background=False)
 
 # %%
 train_tiles = helpers.dataset_generation.generate_candidate_tiles_from_items(
@@ -170,11 +181,20 @@ train_tiles = helpers.dataset_generation.generate_candidate_tiles_from_items(
 test_tiles = helpers.dataset_generation.generate_candidate_tiles_from_items(
     test_dataset, sliding_windows=[sliding_window_dense, sliding_window_sparse], n_jobs=4)
 
+# %% [markdown]
+# ## Toy Dataset Generation
+# Let's generate our first dataset
+
+# %%
+NB_TRAIN_TILES = 400
+NB_TEST_TILE = 100
+
 # %%
 from khumeia.roi.tiles_sampler import *
 # %%
-train_stratified_sampler = StratifiedSampler(
-    nb_tiles_max=400,
+train_stratified_sampler = BackgroundToPositiveRatioSampler(
+    nb_positive_tiles_max=NB_TRAIN_TILES,
+    background_to_positive_ratio=1,
     with_replacement=True,
     shuffle=True,
 )
@@ -183,8 +203,9 @@ train_tiles_sampled = helpers.dataset_generation.sample_tiles_from_candidates(tr
                                                                               tiles_samplers=[train_stratified_sampler])
 
 # %%
-test_stratified_sampler = StratifiedSampler(
-    nb_tiles_max=100,
+test_stratified_sampler = BackgroundToPositiveRatioSampler(
+    nb_positive_tiles_max=NB_TEST_TILE,
+    background_to_positive_ratio=1,
     with_replacement=True,
     shuffle=True,
 )
@@ -200,28 +221,99 @@ train_array = helpers.dataset_generation.dump_dataset_tiles(tiles_dataset=train_
 test_array = helpers.dataset_generation.dump_dataset_tiles(tiles_dataset=test_tiles_sampled, items_dataset=test_dataset)
 
 # %%
-train_images, train_labels = np.asarray([i[0]
-                                         for i in train_array.items]), np.asarray([i[1] for i in train_array.items])
+train_images = np.asarray([i[0] for i in train_array.items])
+train_labels = np.asarray([i[1] for i in train_array.items])
 
 # %%
-test_images, test_labels = np.asarray([i[0] for i in test_array.items]), np.asarray([i[1] for i in test_array.items])
+test_images = np.asarray([i[0] for i in test_array.items])
+test_labels = np.asarray([i[1] for i in test_array.items])
 
 # %%
-test_images.shape
+print(test_images.shape)
+print(test_labels.shape)
 
 # %%
-test_labels.shape
-
-# %%
+# Save as dict of nparrays
 data_dir = os.environ.get("TP_DATA")
 
-with open(os.path.join(data_dir, "train_images.npy"), "wb") as f:
-    np.save(f, train_images)
-with open(os.path.join(data_dir, "train_labels.npy"), "wb") as f:
-    np.save(f, train_labels)
-with open(os.path.join(data_dir, "test_images.npy"), "wb") as f:
-    np.save(f, test_images)
-with open(os.path.join(data_dir, "test_labels.npy"), "wb") as f:
-    np.save(f, test_labels)
+with open(os.path.join(data_dir, "toy_aircraft_dataset.npz"), "wb") as f:
+    np.savez_compressed(f,
+                        train_images=train_images,
+                        train_labels=train_labels,
+                        test_images=test_images,
+                        test_labels=test_labels)
 
 # %%
+# try to reload
+toy_dataset = np.load(os.path.join(data_dir, "toy_aircraft_dataset.npz"))
+train_images = toy_dataset['train_images']
+print(train_images.shape)
+
+# %% [markdown]
+# ## Big Dataset Generation
+# Let's generate our first dataset
+
+# %%
+SAMPLING_RATIO = 9
+NB_TRAIN_TILES = 400
+NB_TEST_TILE = 100
+
+# %%
+from khumeia.roi.tiles_sampler import *
+# %%
+train_stratified_sampler = BackgroundToPositiveRatioPerItemSampler(
+    nb_positive_tiles_max=NB_TRAIN_TILES,
+    background_to_positive_ratio=SAMPLING_RATIO,
+    with_replacement=True,
+    shuffle=True,
+)
+
+train_tiles_sampled = helpers.dataset_generation.sample_tiles_from_candidates(train_tiles,
+                                                                              tiles_samplers=[train_stratified_sampler])
+
+# %%
+test_stratified_sampler = BackgroundToPositiveRatioPerItemSampler(
+    nb_positive_tiles_max=NB_TEST_TILE,
+    background_to_positive_ratio=SAMPLING_RATIO,
+    with_replacement=True,
+    shuffle=True,
+)
+
+test_tiles_sampled = helpers.dataset_generation.sample_tiles_from_candidates(test_tiles,
+                                                                             tiles_samplers=[test_stratified_sampler])
+
+# %%
+train_array = helpers.dataset_generation.dump_dataset_tiles(tiles_dataset=train_tiles_sampled,
+                                                            items_dataset=train_dataset)
+
+# %%
+test_array = helpers.dataset_generation.dump_dataset_tiles(tiles_dataset=test_tiles_sampled, items_dataset=test_dataset)
+
+# %%
+train_images = np.asarray([i[0] for i in train_array.items])
+train_labels = np.asarray([i[1] for i in train_array.items])
+
+# %%
+test_images = np.asarray([i[0] for i in test_array.items])
+test_labels = np.asarray([i[1] for i in test_array.items])
+
+# %%
+print(test_images.shape)
+print(test_labels.shape)
+
+# %%
+# Save as dict of nparrays
+data_dir = os.environ.get("TP_DATA")
+
+with open(os.path.join(data_dir, "aircraft_dataset.npz"), "wb") as f:
+    np.savez_compressed(f,
+                        train_images=train_images,
+                        train_labels=train_labels,
+                        test_images=test_images,
+                        test_labels=test_labels)
+
+# %%
+# try to reload
+toy_dataset = np.load(os.path.join(data_dir, "aircraft_dataset.npz"))
+train_images = toy_dataset['train_images']
+print(train_images.shape)
