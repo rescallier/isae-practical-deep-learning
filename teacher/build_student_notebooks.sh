95d43b90f01932de1122b2357b56f29493c57e88
Florient CHOUTEAU <florient.chouteau@gmail.com>
2019-12-01 22:39:06 +0100

    Update notebooks 2 and 3

--- teacher/build_student_notebooks.sh
+++ teacher/build_student_notebooks.sh
@@ -3,7 +3,7 @@ jupytext --to notebook 1_session_1_hands_on.py -o tmp.ipynb
 nb-filter-cells -i tmp.ipynb -t exercise -o ../1_session_1_hands_on.ipynb
 rm -r tmp.ipynb
 
-jupytext --to notebook 2_session_2_skorch_class_imbalance.py -o tmp.ipynb
+jupytext --to notebook 2_session_2_dealing_with_class_imbalance.py -o tmp.ipynb
 nb-filter-cells -i tmp.ipynb -t exercise -o ../2_session_2_skorch_class_imbalance.ipynb
 rm -r tmp.ipynb
 
